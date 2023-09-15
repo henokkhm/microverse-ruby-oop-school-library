4f9244ebb502910531d77de576ff2c9b76db325e
Henok Kirubel <henokhm2@gmail.com>
2023-09-15 22:47:29 +0300

    Implement Nameable interface in Person class

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,9 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   def initialize(age, name = 'unknown',
                  parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -18,5 +21,9 @@ class Person
 
   def of_age?
     @age && @age >= 18
+  end
+
+  def correct_name
+    @name
   end
 end
